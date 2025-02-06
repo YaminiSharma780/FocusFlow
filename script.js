8c7e4d631e286b075e7a5451d680b5d6d5b0f81e
Yamini <sharmayamini1999@gmail.com>
2025-02-07 03:04:24 +0530

    added checkbox functionality in html and css

--- script.js
+++
@@ -1,8 +0,0 @@
-// const appContainer = document.querySelector(".app-container");
-// const goalContainer1 = document.querySelector("#goal-container");
-
-// const goalContainer2 = goalContainer1.cloneNode(true);
-// appContainer.appendChild(goalContainer2);
-
-// const goalContainer3 = goalContainer1.cloneNode(true);
-// appContainer.appendChild(goalContainer3);
