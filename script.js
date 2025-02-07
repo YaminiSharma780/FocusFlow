81c237f7af7ba0af63b69dfc158a3b6a6c5d7b41
Yamini <sharmayamini1999@gmail.com>
2025-02-07 23:03:08 +0530

    added full working progress bar in javascript

--- script.js
+++ script.js
@@ -36,6 +36,10 @@ const inputFields = document.querySelectorAll(".goal-textbox");
 const errorLabel = document.querySelector(".error-label");
 const progressBar = document.querySelector(".progress-bar");
 const progressValue = document.querySelector(".progress-value");
+const progressSpan = document.querySelector("progress-span");
+
+let barValue = 0;
+let barPercentage = 33.33;
 
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
@@ -44,8 +48,19 @@ checkBoxList.forEach((checkbox) => {
     });
 
     if (allGoalsAdded) {
-      checkbox.parentElement.classList.toggle("completed");
-      progressValue.style.width = "33.33%";
+      if (checkbox.parentElement.classList.contains("completed")) {
+        checkbox.parentElement.classList.remove("completed");
+        barValue--;
+        if (barValue == 0) {
+          progressValue.style.padding = 0;
+        }
+      } else {
+        checkbox.parentElement.classList.add("completed");
+        barValue++;
+        progressValue.style.padding = "0 20px";
+      }
+      progressValue.textContent = `${barValue}/3 Completed`;
+      progressValue.style.width = `${barPercentage * barValue}%`;
     } else {
       progressBar.classList.add("show-error");
     }
